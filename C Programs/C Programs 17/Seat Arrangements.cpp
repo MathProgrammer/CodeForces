d8c41b16cca779d0502152d7a217580e0e776a4c
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-02-02 10:24:07 +0530

    Update Seat Arrangements.cpp

--- C Programs/C Programs 17/Seat Arrangements.cpp
+++ C Programs/C Programs 17/Seat Arrangements.cpp
@@ -16,29 +16,22 @@ int main()
     int left_empty_seats[no_of_rows + 1][no_of_columns + 1];
     memset(left_empty_seats, 0, sizeof(left_empty_seats));
 
+    int total_seatings = 0;
+
     for(int row = 1; row <= no_of_rows; row++)
     {
         for(int column = 1; column <= no_of_columns; column++)
         {
             up_empty_seats[row][column] = (seat[row][column] == '.' ? 1 + up_empty_seats[row - 1][column] : 0);
             left_empty_seats[row][column] = (seat[row][column] == '.' ? 1 + left_empty_seats[row][column - 1] : 0);
+
+            total_seatings += (up_empty_seats[row][column] >= k) + (left_empty_seats[row][column] >= k);
         }
     }
 
-    int seatings[no_of_rows + 1][no_of_columns + 1];
-    memset(seatings, 0, sizeof(seatings));
+    if(k == 1)
+        total_seatings = total_seatings >> 1;
 
-    for(int row = 1; row <= no_of_rows; row++)
-    {
-        for(int column = 1; column <= no_of_columns; column++)
-        {
-            seatings[row][column] = (up_empty_seats[row][column] >= k) + (left_empty_seats[row][column] >= k)
-                                    + seatings[row - 1][column] + seatings[row][column - 1] - seatings[row - 1][column - 1];
-        }
-    }
-
-    int total_seating = (k == 1 ? seatings[no_of_rows][no_of_columns]/2 : seatings[no_of_rows][no_of_columns]);
-
-    printf("%d\n", total_seating);
+    printf("%d\n", total_seatings);
     return 0;
 }
