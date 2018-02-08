f31c441a7aa88ea02e874f8b7db42d630889c7a7
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-02-08 08:59:54 +0530

    Update Cave Painting.cpp

--- C Programs/C Programs 18/Cave Painting.cpp
+++ C Programs/C Programs 18/Cave Painting.cpp
@@ -6,7 +6,7 @@ int main()
     scanf("%I64d %I64d", &n, &k);
 
     const int LIMIT = 43;
-    int divisible_by_lcm = true;
+    int divisible_by_lcm_1_till_k = true;
 
     if(k < LIMIT)
     {
@@ -14,12 +14,12 @@ int main()
         {
             if( (n + 1)%i != 0)
             {
-                divisible_by_lcm = false;
+                divisible_by_lcm_1_till_k = false;
                 break;
             }
         }
     }
 
-    printf(k < LIMIT && divisible_by_lcm ? "Yes\n" : "No\n");
+    printf(k < LIMIT && divisible_by_lcm_1_till_k ? "Yes\n" : "No\n");
     return 0;
 }
