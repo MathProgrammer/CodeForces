b1ae530095c45a99a64563dc17cc9098976ab9d7
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-09-03 10:18:59 +0530

    Add files via upload

---
+++ Contests/Manthan 2018/Programs/Reach Median.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+#define all(v) (v).begin() + 1, (v).end()
+using namespace std;
+
+int main()
+{
+    int no_of_elements, median_target;
+    cin >> no_of_elements >> median_target;
+
+    vector <int> A(no_of_elements + 1);
+    for(int i = 1; i <= no_of_elements; i++)
+        cin >> A[i];
+
+    sort(all(A));
+
+    int middle = no_of_elements/2 + 1;
+    long long no_of_operations = 0;
+
+    for(int i = 1; i <= no_of_elements; i++)
+    {
+        if(i < middle)
+        {
+            if(A[i] > median_target)
+            {
+                no_of_operations += A[i] - median_target;
+            }
+        }
+        else if(i == middle)
+        {
+            no_of_operations += abs(A[i] - median_target);
+        }
+        else if(i > middle)
+        {
+            if(A[i] < median_target)
+            {
+                no_of_operations += median_target - A[i];
+            }
+        }
+    }
+
+    cout << no_of_operations;
+    return 0;
+}
+
