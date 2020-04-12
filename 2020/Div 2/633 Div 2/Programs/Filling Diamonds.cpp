44fef11fb762eeed6597c9a02772d89258e9f759
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2020-04-12 22:27:17 +0530

    Create Filling Diamonds.cpp

---
+++ 2020/Div 2/633 Div 2/Programs/Filling Diamonds.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve()
+{
+    long long n;
+    cin >> n;
+    
+    cout << n << "\n";
+}
+
+int main()
+{
+    int no_of_test_cases;
+    cin >> no_of_test_cases;
+    
+    while(no_of_test_cases--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
